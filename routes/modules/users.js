aed08c3b6f6a3877414cff50b9553d5a46376352
cotool <tusky.saga@gmail.com>
2020-08-20 22:14:34 +0800

    feat: add user login page

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -3,4 +3,12 @@ const router = express.Router()
 router.get('/login',(req,res) => {
     res.render('login')
 })
+//login頁面 from傳來的
+router.get('/login',(req,res) => {
+    console.log('login')
+})
+//註冊
+router.get('/register',(req,res) => {
+    res.render('register')
+})
 module.exports = router
