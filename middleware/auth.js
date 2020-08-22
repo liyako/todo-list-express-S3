c81ab8d2ea8e01c87492d8095493c9a427ec204c
cotool <tusky.saga@gmail.com>
2020-08-22 17:16:34 +0800

    feat: add luser authentication in todo and home routers

---
+++ middleware/auth.js
@@ -0,0 +1,10 @@
+const { model } = require("mongoose");
+
+module.exports = {
+    authenticator: (req,res,next) => {
+        if (req.isAuthenticated()) {
+            return next()
+        }
+        res.redirect('/users/login')
+    }
+}
--- routes/index.js
+++ routes/index.js
@@ -4,9 +4,10 @@ const router = express.Router()
 const home = require('./modules/home')
 const todos = require('./modules/todos')
 const users = require('./modules/users')
+const {authenticator} = require('../middleware/auth')//掛載 middleware
 
-router.use('/', home)
-router.use('/todos', todos)
+router.use('/todos',authenticator, todos)
 router.use('/users', users)
+router.use('/',authenticator, home)// 加入驗證程序,定義寬鬆的路由引到清單最下方
 
 module.exports = router
